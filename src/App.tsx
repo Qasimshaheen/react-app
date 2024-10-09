9f81edd92c348ba0729eb436db6847b125cbf4b4
Qasim Shaheen <qasim.s@applab.qa>
2024-10-09 15:59:21 +0500

    Added a Like component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
-import { Alert } from "./components/Alert";
-import { Button } from "./components/Button/Button";
+import { Like } from "./components/Like";
 
-const handleClick = () => {
-  console.log("clicked");
-};
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisible(true)}>Clicke Me!</Button>
+      <Like onClick={() => console.log("clicked!")} />
     </div>
   );
 }
---
+++ src/components/Like.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+interface Props {
+  onClick: () => void;
+}
+
+export const Like = ({ onClick }: Props) => {
+  const [status, setStatus] = useState(false);
+  const toggle = () => {
+    setStatus(!status);
+    onClick();
+  };
+  if (status) return <AiFillHeart color="#ff6b81" size={20} onClick={toggle} />;
+  else return <AiOutlineHeart color="#fffff" size={20} onClick={toggle} />;
+};
