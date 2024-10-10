890f7ff33cc49696d27a1881f7ebcb3b6d2fcd52
Qasim Shaheen <qasim.s@applab.qa>
2024-10-10 18:24:25 +0500

    Building Expense List

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
-import { Form } from "./components/Form";
+import { ExpenseList } from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "production 1", amount: 10, category: "Utilities" },
+    { id: 2, description: "production 2", amount: 30, category: "Utilities" },
+    { id: 3, description: "production 3", amount: 50, category: "Utilities" },
+    { id: 4, description: "production 4", amount: 60, category: "Utilities" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((e) => e.id !== id));
+  };
   return (
     <div>
-      <Form></Form>
+      <ExpenseList expenses={expenses} onDelete={(id) => handleDelete(id)} />
     </div>
   );
 }
