496c8eaaa6f9f2f2520362d9b45374f4863a3779
Qasim Shaheen <qasim.s@applab.qa>
2024-10-10 10:59:10 +0500

    Sharing state between components

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface Props {
+  cartItemsCount: number;
+}
+export const NavBar = ({ cartItemsCount }: Props) => {
+  return <div>NavBar: {cartItemsCount}</div>;
+};
