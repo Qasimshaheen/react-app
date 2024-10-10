5362aeeef45e3e55595f035859a6bc089df8f913
Qasim Shaheen <qasim.s@applab.qa>
2024-10-10 11:53:45 +0500

    update state example 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "cheez"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
   return (
     <div>
-      {pizza.toppings.map((t) => ` ${t}`)}
+      {cart.items.map((c) => `${c.quantity} `)}
       <br />
       <button onClick={handleClick}>Update</button>
     </div>
