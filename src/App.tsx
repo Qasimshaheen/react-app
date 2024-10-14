ae1a6b68661c8f718613eb8fb748fd3bc61431f5
Qasim Shaheen <qasim.s@applab.qa>
2024-10-14 16:24:12 +0500

    fetching users data from open source api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { useEffect } from "react";
-
-const connect = () => console.log("connecting");
-const disconnect = () => console.log("Disconnecting");
+import axios from "axios";
+import { useEffect, useState } from "react";
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    connect();
-    return () => disconnect();
-  });
-  return <div></div>;
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+  return (
+    <ul>
+      {users.map((u) => (
+        <li key={u.id}>{u.name}</li>
+      ))}
+    </ul>
+  );
 }
 export default App;
